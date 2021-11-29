58fc8a2d3b10bdf3dc1674542af4790b7ea809ae
na2shell <nagafeltf7@gmail.com>
2021-11-29 18:41:33 +0900

    blackでフォーマッタをかけた 分析ツールを自動で画像を保存するように変更

--- priority_queue/client/src/declare_priority_queue.py
+++ priority_queue/client/src/declare_priority_queue.py
@@ -6,41 +6,51 @@ import uuid
 import subprocess
 
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='broker', port=5672))
+    pika.ConnectionParameters(host="broker", port=5672)
+)
 channel = connection.channel()
 
 args = {"x-max-priority": 5}
-channel.queue_declare(queue='task2', arguments=args)
+channel.queue_declare(queue="task2", arguments=args)
+
 
 def logger(mes):
     fw = open("../log/log_send.txt", "a")
     fw.write("%s\n" % (mes))
 
-def p_message(n,mes,_priority):
+
+def p_message(n, mes, _priority):
     channel.basic_publish(
-        exchange='',
-        routing_key='task2',
+        exchange="",
+        routing_key="task2",
         body=mes,
         properties=pika.BasicProperties(
             priority=_priority,
             delivery_mode=1,  # make message persistent
-        ))
-    print(" [%d] Sent %r" % (n,mes))
+        ),
+    )
+    print(" [%d] Sent %r" % (n, mes))
     logger(mes)
 
+
 def make_file(vol):
-    vol = "10k"
-    filename = str(uuid.uuid4())+".dat"
-    path = "/home/data/"+filename
-    subprocess.run(["fallocate", "-l", vol,path])
+    filename = str(uuid.uuid4()) + ".dat"
+    path = "/home/data/" + filename
+    subprocess.run(["fallocate", "-l", vol, path])
     return filename
+
 
 if __name__ == "__main__":
     for i in range(100):
-        p = random.randint(1,5)
-        filename = make_file("10k")
-        mes = ",".join([str(time.time()),str(p),filename])
-        p_message(i,mes, p)
-        #time.sleep(0.1)
+        p = random.randint(1, 5)
+
+        if p > 3:
+            filename = make_file("10k")
+        else:
+            filename = make_file("500b")
+
+        mes = ",".join([str(time.time()), str(p), filename])
+        p_message(i, mes, p)
+        # time.sleep(0.1)
 
     connection.close()
--- priority_queue/client/src/receive.py
+++ priority_queue/client/src/receive.py
@@ -6,48 +6,51 @@ import os
 import requests
 
 
-def logger(pubtime,priority,kind,file_name):
+def logger(pubtime, priority, kind, file_name):
     fw = open("../log/log.txt", "a")
-    fw.write("%f [%s] Received %s %f %r \n" % (time.time(), file_name,kind, pubtime, priority))
+    fw.write(
+        "%f [%s] Received %s %f %r \n"
+        % (time.time(), file_name, kind, pubtime, priority)
+    )
 
-def http_request(file_name,logger,pubtime,priority):
-    url = "http://web/"+file_name
-    file_path = "/home/download/"+file_name
+
+def http_request(file_name, logger, pubtime, priority):
+    url = "http://web/" + file_name
+    file_path = "/home/download/" + file_name
     res = requests.get(url, stream=True)
     if res.status_code != 200:
         raise ValueError("error!")
 
-    with open(file_path, 'wb') as file:
+    with open(file_path, "wb") as file:
         for chunk in res.iter_content(chunk_size=1024):
             file.write(chunk)
-    
-    print("suc: ",file_name)
-    logger(pubtime,priority,"HTTP",file_name)
 
+    print("suc: ", file_name)
+    logger(pubtime, priority, "HTTP", file_name)
 
 
 if __name__ == "__main__":
     try:
         os.remove("../log/log.txt")
         connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='broker', port=5672))
+            pika.ConnectionParameters(host="broker", port=5672)
+        )
         channel = connection.channel()
         st_time = time.time()
-        print(' [*] Waiting for messages. To exit press CTRL+C')
+        print(" [*] Waiting for messages. To exit press CTRL+C")
 
         def callback(ch, method, properties, body):
             tmp = body.decode().split(",")
             pubtime = float(tmp[0])
             priority = int(tmp[1])
             file_name = tmp[2]
-            print("%f [x] Received %f %r \n" %
-                  (time.time(),pubtime, priority))
-            logger(pubtime, priority,"MQPs",file_name)
+            print("%f [x] Received %f %r \n" % (time.time(), pubtime, priority))
+            logger(pubtime, priority, "MQPs", file_name)
             ch.basic_ack(delivery_tag=method.delivery_tag)
-            http_request(file_name,logger,pubtime, priority)
+            http_request(file_name, logger, pubtime, priority)
 
         channel.basic_qos(prefetch_count=5)
-        channel.basic_consume(queue='task2', on_message_callback=callback)
+        channel.basic_consume(queue="task2", on_message_callback=callback)
         channel.start_consuming()
     except KeyboardInterrupt:
         print("==> catch interapt")
